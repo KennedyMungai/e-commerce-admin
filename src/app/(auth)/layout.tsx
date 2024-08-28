b9146f2e25ba64e89e1197e7da3109c1ca5047f9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-28 15:46:46 +0300

    Added some styling for the auth layout page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,15 +8,20 @@ type Props = {
 const AuthLayout = ({ children }: Props) => {
 	return (
 		<div className='h-full grid grid-cols-2'>
-			<div className='flex justify-center items-center'>
+			<div className='flex flex-col justify-center items-center gap-y-4'>
 				<Image
-					src={'/logo.png'}
+					src={'/logo.svg'}
 					alt={'logo'}
 					width={200}
 					height={200}
 				/>
+				<p className='font-semibold text-3xl text-indigo-600'>
+					E-Commerce
+				</p>
 			</div>
-			<div className='flex items-center justify-center'>{children}</div>
+			<div className='flex items-center justify-center bg-indigo-600'>
+				{children}
+			</div>
 		</div>
 	)
 }
