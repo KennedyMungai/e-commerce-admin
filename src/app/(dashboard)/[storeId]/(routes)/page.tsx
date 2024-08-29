b5b5c88ad64b186da0e43b28afd7f631a0566719
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-29 12:38:48 +0300

    Created the dashboard organizational route

---
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,11 @@
+type Props = {
+	params: {
+		storeId: string
+	}
+}
+
+const DashboardPage = ({ params: { storeId } }: Props) => {
+	return <div>StorePage</div>
+}
+
+export default DashboardPage
---
+++ src/app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,35 @@
+import { db } from '@/db'
+import { store } from '@/db/schema'
+import { auth } from '@clerk/nextjs/server'
+import { and, eq } from 'drizzle-orm'
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
+
+type Props = {
+	children: ReactNode
+	params: {
+		storeId: string
+	}
+}
+
+const DashboardLayout = async ({ children, params: { storeId } }: Props) => {
+	const { userId } = auth()
+
+	if (!userId) redirect('/sign-in')
+
+	const data = await db.query.store.findFirst({
+		where: and(eq(store.id, storeId), eq(store.userId, userId)),
+		columns: { id: true, name: true }
+	})
+
+	if (!data) redirect('/')
+
+	return (
+		<>
+			<div>This will be a navbar</div>
+			<div>{children}</div>
+		</>
+	)
+}
+
+export default DashboardLayout
