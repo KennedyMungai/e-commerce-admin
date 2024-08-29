f8f77bb8e123f4cb9bf445a7df87ede7a5a7e086
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-29 15:24:40 +0300

    Created the main nav component and added it to the navbar component

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,4 +1,5 @@
 import { UserButton } from '@clerk/nextjs'
+import MainNav from '@/components/main-nav'
 
 type Props = {}
 
@@ -7,7 +8,7 @@ const NavBar = () => {
 		<div className='border-b'>
 			<div className='flex h-16 items-center px-4'>
 				<div>This will be a store switcher</div>
-				<div>This will be the routes</div>
+				<MainNav className='mx-6' />
 				<div className='ml-auto flex space-x-4'>
 					<UserButton />
 				</div>
