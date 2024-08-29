f43a39cfe56a45cbc33fdd7f0ed7cf095f1e9444
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-29 11:43:17 +0300

    Created the route file for the stores api

---
+++ src/app/api/stores/route.ts
@@ -0,0 +1,28 @@
+import { db } from '@/db'
+import { store } from '@/db/schema'
+import { auth } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+
+export const POST = async (req: Request) => {
+	try {
+		const { userId } = auth()
+		const body = await req.json()
+
+		const { name } = body
+
+		if (!userId) return new NextResponse('Unauthorized', { status: 401 })
+
+		if (!name) return new NextResponse('Name is required', { status: 400 })
+
+		const data = await db.insert(store).values({
+			name,
+			userId
+		})
+
+		return NextResponse.json(data)
+	} catch (error: any) {
+		console.log('[STORE_POST]', error.message)
+
+		return new NextResponse(error.message, { status: 500 })
+	}
+}
