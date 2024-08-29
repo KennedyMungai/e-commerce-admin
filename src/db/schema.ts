2266a6b10116d316ba4614b1f5412a619ccf3b60
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-29 11:20:14 +0300

    Created a simple schema for creating stores

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,9 @@
-import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
+import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core'
 
 export const store = pgTable('store', {
 	id: uuid('id').defaultRandom().primaryKey(),
-	name: varchar('name', { length: 255 }).notNull()
+	name: varchar('name', { length: 255 }).notNull(),
+	userId: varchar('userId', { length: 255 }).notNull(),
+	createdAt: timestamp('createdAt').defaultNow().notNull(),
+	updatedAt: timestamp('updatedAt').$onUpdate(() => new Date())
 })
