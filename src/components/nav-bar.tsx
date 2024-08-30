9ee379daf3e81f73c79f4dc86e9f9db436243161
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-30 09:23:46 +0300

    Fetching the stores from the database

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,14 +1,27 @@
 import MainNav from '@/components/main-nav'
 import { StoreSwitcher } from '@/components/store-switcher'
+import { db } from '@/db'
+import { Store } from '@/db/schema'
 import { UserButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
+import { eq } from 'drizzle-orm'
+import { redirect } from 'next/navigation'
 
 type Props = {}
 
-const NavBar = () => {
+const NavBar = async () => {
+	const { userId } = auth()
+
+	if (!userId) redirect('/sign-in')
+
+	const stores = await db.query.Store.findMany({
+		where: eq(Store.userId, userId)
+	})
+
 	return (
 		<div className='border-b'>
 			<div className='flex h-16 items-center px-4'>
-				<StoreSwitcher />
+				<StoreSwitcher items={stores} />
 				<MainNav className='mx-6' />
 				<div className='ml-auto flex space-x-4'>
 					<UserButton />
