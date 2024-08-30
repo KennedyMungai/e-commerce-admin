984d645c22780f604c6e62703e7c9f7d511cf95d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-08-30 15:08:45 +0300

    Added the billboard table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
+import { relations } from 'drizzle-orm'
 import { pgTable, timestamp, uuid, varchar } from 'drizzle-orm/pg-core'
 import { createInsertSchema } from 'drizzle-zod'
-import { z } from 'zod'
 
 export const Store = pgTable('store', {
 	id: uuid('id').defaultRandom().primaryKey(),
@@ -10,4 +10,23 @@ export const Store = pgTable('store', {
 	updatedAt: timestamp('updatedAt').$onUpdate(() => new Date())
 })
 
+export const StoreRelations = relations(Store, ({ many }) => ({
+	Store: many(BillBoard)
+}))
+
 export const createStoreSchema = createInsertSchema(Store)
+
+export const BillBoard = pgTable('billboard', {
+	id: uuid('id').defaultRandom().primaryKey(),
+	storeId: uuid('storeId')
+		.references(() => Store.id)
+		.notNull(),
+	label: varchar('label', { length: 255 }).notNull(),
+	imageUrl: varchar('imageUrl', { length: 255 }).notNull(),
+	createdAt: timestamp('createdAt').defaultNow().notNull(),
+	updatedAt: timestamp('updatedAt').$onUpdate(() => new Date())
+})
+
+export const BillboardRelations = relations(BillBoard, ({ one }) => ({
+	store: one(Store, { fields: [BillBoard.storeId], references: [Store.id] })
+}))
